091465baa8b13bdf26cad52c6c30a14ba88e9dab
chellesjord <m.kayejordan@gmail.com>
2022-04-02 23:29:25 -0500

    add schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,75 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth')
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+        }
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError("Can't find this user");
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        //TODO: fix save book resolver it's not right, but i need to come back to it
+    //     saveBook: async (parent, { bookId }, context) => {
+    //         if (context.user) {
+    //             const updatedUser = await User.findOneAndUpdate(
+    //                 { _id: bookId },
+    //                 { $push: { reactions: { reactionBody, username: context.user.username } } },
+    //                 { new: true, runValidators: true }
+    //             );
+
+    //             return updatedUser;
+    //         }
+
+    //         throw new AuthenticationError('You need to be logged in!');
+    //     },
+    //     // TODO: work on remove book resolver
+    //     removeBook: async (parent, { bookId }, context) => {
+    //         if (context.user) {
+    //             const updatedUser = await User.findByIdAndUpdate(
+    //                 { _id: context.user._id },
+    //                 { $pull: { savedBooks: { bookId } } },
+    //                 { new: true }
+    //             );
+    //             return updatedUser;
+    //         }
+    //         throw new AuthenticationError('You need to be logged in!');
+    //     },
+    }
+}
