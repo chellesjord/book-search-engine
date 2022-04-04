c5287ab7612224d68d7b4300dd7280e2f109e80f
chellesjord <m.kayejordan@gmail.com>
2022-04-03 23:13:33 -0500

    updated README file

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# book-search-engine
+# Employee-Tracker 
+
+GitHub Link: https://github.com/chellesjord/book-search-engine
+
+Deployed Link: https://search-engine-book-1.herokuapp.com/
+
+  ## Description:
+  The user is able to search for new books to read so they can keep a list of books to purchase
+
+  ## Table of Contents:
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Questions](#questions)
+
+  ## Installation
+  The user needs to have Node.js, Inquirer, and MySQL2 downloaded to run this program. 
+
+  In the command prompt type "npm i" to install all programs for this application
+
+
+  ## Usage
+  
+
+  ## Questions:
+  Check out my [GitHub page](https://github.com/chellesjord)!
+
+  If you have additonal questions email me at 
+  <a href="mailto:m.kayejordan@gmail.com">m.kayejordan@gmail.com</a>.
+  
+
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
-// TODO: 
-// Remove the useEffect() Hook that sets the state for UserData.
-
-// Instead, use the useQuery() Hook to execute the GET_ME query on load and save it to a variable named userData.
-
-// Use the useMutation() Hook to execute the REMOVE_BOOK mutation in the handleDeleteBook() function instead of the deleteBook() function that's imported from API file. (Make sure you keep the removeBookId() function in place!)
-
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(GET_ME);
+  const [removeBook, {error}] = useMutation(REMOVE_BOOK);
+  const userData = data?.me || {} ;
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
