604db160ab3caafce9d9049d964c29c4cc0075cc
chellesjord <m.kayejordan@gmail.com>
2022-04-05 06:40:12 -0500

    import apollo and usestate

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -10,7 +10,7 @@ import { removeBookId } from '../utils/localStorage';
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
   const [removeBook, {error}] = useMutation(REMOVE_BOOK);
-  const userData = data?.me || {} ;
+  const [userData, setUserData] = data?.me || {} ;
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
